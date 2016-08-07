5c529bcef40d8b608a470106deaa1fc569211278
BornToRain <1159930219@qq.com>
2016-08-07 15:45:51 +0800

    Test

--- src/main/java/own/ryze/application/weixin/persistent/dao/UserDao.java
+++ src/main/java/own/ryze/application/weixin/persistent/dao/UserDao.java
@@ -3,14 +3,10 @@ package own.ryze.application.weixin.persistent.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import own.ryze.application.weixin.persistent.bean.User;
 
-@RepositoryRestResource(path = "/users", collectionResourceRel = "/users")
-public interface UserDao extends PagingAndSortingRepository<User, Long>
+public interface UserDao extends JpaRepository<User, Long>
 {
 	User findByUsernameAndPassword(String username, String password);
 
